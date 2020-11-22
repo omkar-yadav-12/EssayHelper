1e9fda18208a3d665c24e6ec121ac7976ee91fcc
Omkar Yadav <omyad21@icstudents.org>
2020-11-22 17:52:03 -0600

    Initial synonym bot

--- index.js
+++ index.js
@@ -2,18 +2,34 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.TOKEN;
-
+const thesaurus = require('thesaurus');
+const word_definition = require('word-definition');
 bot.login(TOKEN);
 
 bot.on('ready', () => {
   console.info(`Logged in as ${bot.user.tag}!`);
 });
 
-bot.on('message', msg => {
+function definition (word) {
+  word_definition.getDef(word, "en", null, function(definition) {
+    console.log(definition)
+    return definition
+  })
+}
+
+bot.on('message', async msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
     msg.channel.send('pong');
-
+  } else if (msg.content.startsWith('synonym')) {
+    const word = msg.content.substring(7);
+    const synonyms = thesaurus.find(word.trim());
+    let response = "";
+    // for (synonym in synonyms) {
+    //   console.log(await definition(synonym))
+    // }
+    if (synonyms.length > 0) msg.channel.send(synonyms);
+    else msg.channel.send(`Looks like we don't have any synonyms for ${word} :(`)
   } else if (msg.content.startsWith('!kick')) {
     if (msg.mentions.users.size) {
       const taggedUser = msg.mentions.users.first();
