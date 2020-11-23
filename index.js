e136061dd60e65aed90d848e100de76e443a1cc3
Omkar Yadav <omyad21@icstudents.org>
2020-11-23 16:39:48 -0600

    base conversion and gcd

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-const TOKEN = process.env.DIS_TOKEN;
+const essay_bot = new Discord.Client();
+const discrete_bot = new Discord.Client();
+const ESSAY_TOKEN = process.env.ESSAY_TOKEN;
+const DISCRETE_TOKEN = process.env.DISCRETE_TOKEN;
 const thesaurus = require('thesaurus');
 const fetch = require('node-fetch');
 const url = 'https://owlbot.info/api/v4/dictionary/';
@@ -19,13 +21,13 @@ get_def = async (word) => {
   return await data.json();
 }
 
-bot.login(TOKEN);
+essay_bot.login(ESSAY_TOKEN);
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+essay_bot.on('ready', () => {
+  console.info(`Logged in as ${essay_bot.user.tag}!`);
 });
 
-bot.on('message', async msg => {
+essay_bot.on('message', async msg => {
   if (msg.content.startsWith('synonym')) {
     const word = msg.content.substring(7);
     const synonyms = thesaurus.find(word.trim());
@@ -33,7 +35,7 @@ bot.on('message', async msg => {
     else msg.channel.send(`Looks like we don't have any synonyms for ${word} :(`)
   } else if (msg.content.startsWith('def')) {
     const data = await get_def(msg.content.substring(3).trim());
-    let message = "";
+    let message = '';
     if (data[0] === undefined) {
       message += `Pronunciation: ${data.pronunciation}\nWord: ${data.word}\n\n`;
       for (let i = 0; i < data.definitions.length; i++) {
@@ -43,3 +45,31 @@ bot.on('message', async msg => {
     msg.channel.send(message);
   }
 });
+
+
+discrete_bot.login(DISCRETE_TOKEN);
+
+discrete_bot.on('ready', () => {
+  console.info(`Logged in as ${discrete_bot.user.tag}!`);
+});
+
+gcd = (a, b) => {
+  if (!b) return a;
+  else return gcd(b, a % b);
+};
+
+discrete_bot.on('message',  msg => {
+  if (msg.content.toUpperCase().startsWith('!gcd')) {
+    let inputs = msg.content.substring(4).trim();
+    inputs = inputs.split(' ');
+    msg.channel.send(gcd(parseInt(inputs[0]), parseInt(inputs[1])));
+  } else if (msg.content.toLowerCase().startsWith('!conversion')) { 
+    let inputs = msg.content.substring(11).trim();
+    inputs = inputs.split(',');
+    msg.channel.send(parseInt(inputs[0], inputs[1]));
+  } else if (msg.content.toLocaleLowerCase().startsWith('!reverseconversion')) {
+    let inputs = msg.content.substring(18).trim();
+    inputs = inputs.split(',');
+    msg.channel.send((inputs[0]>>>0).toString(inputs[1]));
+  }
+})
