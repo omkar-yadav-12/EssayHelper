e63eca655507ad094957c5a25d666065d0e84ef4
Omkar Yadav <omyad21@icstudents.org>
2020-11-22 19:15:01 -0600

    token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const TOKEN = "NzgwMTgxNDgwMTg5NzIyNjM1.X7rWnw.cF9xPgfwWO-nsfEgBn0upp0avTw";
 const thesaurus = require('thesaurus');
 const word_definition = require('word-definition');
 bot.login(TOKEN);
